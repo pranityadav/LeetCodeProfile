1bb0d6afe34f4bb49060370f5134bd74f1cb12e7
Huma97-crypto <59989943+Huma97-crypto@users.noreply.github.com>
2020-12-14 10:02:15 +0500

    Time: 16 ms (52.58%), Space: 8.6 MB (35.17%) - LeetHub

---
+++ knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double knightProbability(int N, int K, int sr, int sc) {
+        vector<vector<double>> dp(N , vector<double> (N, 0.0));
+        int dr[8] = {2, 2, 1, 1, -1, -1, -2, -2};
+        int dc[8] = {1, -1 , 2, -2, 2,-2,  1, -1};
+        dp[sr][sc] = 1;
+        for(; K>0;K--){
+            vector<vector<double>> dp2(N , vector<double> (N, 0.0));
+            for(int r = 0; r<N ; r++){
+                for(int c = 0; c<N; c++){
+                    for(int k=0; k<8; k++){
+                        int cr = r + dr[k];
+                        int cc = c+ dc[k];
+                        if(0 <=cr && cr<N && 0<=cc && cc<N){
+                            dp2[cr][cc] +=dp[r][c]/8.0;
+                        }
+                    }
+                }
+            }
+            dp = dp2;
+        }
+        double ans = 0.0;
+        for(vector<double> row: dp){
+            for(double x:row){
+                ans+=x;
+            }
+        }
+        return ans;
+    }
+};
