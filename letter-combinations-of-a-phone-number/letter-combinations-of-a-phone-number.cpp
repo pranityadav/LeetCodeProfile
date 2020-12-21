5a3961ed761c670fafc97bb8dfd8d3d166f88d85
Huma97-crypto <59989943+Huma97-crypto@users.noreply.github.com>
2020-12-21 22:37:20 +0500

    Time: 0 ms (100.00%), Space: 6.9 MB (55.45%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,21 +1,22 @@
 class Solution {
+    vector<string> nums ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+     vector<string> ans;
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if(digits.length() == 0) return ans;
-        vector<string> nums ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        rec(digits,ans,nums,0,"");
+       
+        if(digits.length() == 0) return ans;       
+        rec(digits,0,"");
         return ans;
         
     }
-    void rec(string digits, vector<string> &ans,vector<string> &nums, int i, string s){
+    void rec(string &digits, int i, string s){
         if( i == digits.length()){
             ans.push_back(s);
             return;
         }
         string val = nums[digits[i] - '0'];
         for(int j =0; j<val.size(); j++){
-            rec(digits,ans,nums,i+1,s+val[j]);
+            rec(digits,i+1,s+val[j]);
         }
     }
 };
