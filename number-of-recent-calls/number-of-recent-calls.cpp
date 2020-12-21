5bb3666a8173137fb47bb06b33c97a5c7d4933eb
Huma97-crypto <59989943+Huma97-crypto@users.noreply.github.com>
2020-12-21 17:00:33 +0500

    Time: 200 ms (73.20%), Space: 57.9 MB (33.56%) - LeetHub

---
+++ number-of-recent-calls/number-of-recent-calls.cpp
@@ -0,0 +1,25 @@
+class RecentCounter {
+public:
+    queue<int>pings;
+    int numPings;
+    
+    RecentCounter() {
+       numPings  = 0;
+    }
+    
+    int ping(int t) {
+         while (!pings.empty() and (t - pings.front()) > 3000) {
+            pings.pop();
+            numPings--;
+        }
+        pings.push (t);
+        numPings++;
+        return numPings;
+    }
+};
+​
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
