1c5cbbeab469eb59fef304284a894b939f84a48e
Huma97-crypto <59989943+Huma97-crypto@users.noreply.github.com>
2020-12-25 17:24:04 +0500

    Time: 24 ms (50.52%), Space: 10.8 MB (9.43%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-         priority_queue<int, vector<int>, greater<int>> pq;
-​
-        for(int n:nums){
-            pq.push(n);
-            if(pq.size()>k){
-                pq.pop();
-            }
-        }
-       
-        return pq.top();
+       priority_queue<int> pq;
+       for(int n:nums){
+           pq.push(n);
+       } 
+       for(int i = 1; i<=k-1 ; i++){
+           pq.pop();
+       } 
+       return pq.top() ;
     }
     
 };
