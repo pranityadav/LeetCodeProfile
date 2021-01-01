adaf72cd59351c5288b134367fd2e72034dcdaee
Huma97-crypto <59989943+Huma97-crypto@users.noreply.github.com>
2021-01-01 17:41:30 +0500

    Time: 0 ms (100.00%), Space: 6.1 MB (77.69%) - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -4,19 +4,17 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int l=1;
-        int r=n;
+        int l = 1;
+        int r = n;
         int mid = l + (r-l)/2;
-        while(l<r)
-        {    
-             
-            if(isBadVersion(mid))
-            {
-                r=mid;
-            }
-            else l=mid+1;
-             mid=l+(r-l)/2;
+        while(l<r){
            
+            if(isBadVersion(mid)){
+                r = mid;
+            }
+            else
+                l = mid+1;
+            mid = l + (r-l)/2; 
         }
         return mid;
     }
