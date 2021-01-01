9f6019daf5b4643916c49585e797e19de64b67a3
Huma97-crypto <59989943+Huma97-crypto@users.noreply.github.com>
2021-01-01 16:59:38 +0500

    Time: 0 ms (100.00%), Space: 6.3 MB (46.11%) - LeetHub

---
+++ first-bad-version/first-bad-version.cpp
@@ -0,0 +1,23 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+​
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l=1;
+        int r=n;
+        int mid = l + (r-l)/2;
+        while(l<r)
+        {    
+             
+            if(isBadVersion(mid))
+            {
+                r=mid;
+            }
+            else l=mid+1;
+             mid=l+(r-l)/2;
+           
+        }
+        return mid;
+    }
+};
