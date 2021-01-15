65d3fde0d9a93d1af851289fc5d563340b408f94
Huma97-crypto <59989943+Huma97-crypto@users.noreply.github.com>
2021-01-15 16:58:12 +0500

    Time: 40 ms (65.86%), Space: 14.6 MB (13.20%) - LeetHub

---
+++ running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        ans = []
+        sum = 0
+        for n in nums:
+            sum = sum + n
+            ans.append(sum)
+            
+        return ans    
