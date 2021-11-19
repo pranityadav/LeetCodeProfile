e5929919b4d162c57bacb8ccd6ec39ef49f22f90
Huma Jamil <59989943+Huma97-crypto@users.noreply.github.com>
2021-11-18 18:56:39 -0700

    Time: 40 ms (44.18%), Space: 27.6 MB (61.99%) - LeetHub

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1, mid;
-        while (l <= r) {
-            mid = (l + r) / 2;
-            if (nums[mid] == target) break;
-            else if (nums[mid] < target) l = mid + 1;
-            else r = mid - 1;
-        }
-        return nums[mid] == target ? mid : -1;
-    }
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if (target < nums[mid]) {
+                right = mid-1;
+            }   
+            else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 };
