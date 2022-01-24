6e4606b0d054d412667e449d39683aa2ec840da7
Huma Jamil <59989943+Huma97-crypto@users.noreply.github.com>
2022-01-23 21:12:33 -0700

    Time: 18 ms (11.92%), Space: 14.9 MB (44.26%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,21 +11,13 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* head = NULL;
-        ListNode* temp = NULL;
+        ListNode* head = new ListNode;
+        ListNode* temp = head;
         
-        if(!list1) return list2;
-        if(!list2) return list1;
-        if(list1->val <= list2->val){
-            head = list1;
-            
-            list1 = list1->next;
-        }
-        else{
-            head = list2;
-            list2 = list2->next;
-        }
-            temp = head;
+        //if(!list1) return list2;
+        //if(!list2) return list1;
+        //if(list1->val <= list2->val){
+        
         while(list1 && list2){
             if(list1->val <= list2->val){
             temp->next = list1;
@@ -36,9 +28,6 @@ public:
             list2 = list2->next;
             }
             temp = temp -> next;
-            
-            
-            
         }
         if(list1){
             temp ->next = list1;
@@ -46,6 +35,6 @@ public:
         if(list2) 
             temp->next = list2;
         
-        return head;
+        return head->next;
     }
 };
